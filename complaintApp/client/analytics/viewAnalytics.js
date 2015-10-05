49945f9b61077fb142c938a8e1eb4d22c9a28aa3
Ong Zhi Min <zhimin_93@hotmail.sg>
2015-10-05 18:07:44 +0800

    Analytics Page with dummy charts and data

--- complaintApp/client/analytics/viewAnalytics.js
+++ complaintApp/client/analytics/viewAnalytics.js
@@ -1,5 +1,76 @@
 if (Meteor.isClient) {
-	Template.viewAnalytics.events({
-		
+	Template.viewAnalytics.onRendered(function(){
+        var chart1 = c3.generate({
+            bindto: '#complaints',
+            data: {
+              columns: [
+                ['Total Number of Complaints', 30, 200, 100, 400, 150, 250]
+              ],
+              type: 'bar'
+            },
+            grid: {
+                x: {
+                    show: true
+                },
+                y: {
+                    show: true
+                }
+            }
+        });
+
+        var chart2 = c3.generate({
+            bindto: '#complaints-per-cat',
+            data: {
+              columns: [
+                ['Total Number of Complaints Per Category', 50, 150, 80, 200, 140, 40]
+              ],
+              type: 'bar'
+            },
+            grid: {
+                x: {
+                    show: true
+                },
+                y: {
+                    show: true
+                }
+            }
+        });
+
+        var chart3 = c3.generate({
+            bindto: '#compliments',
+            data: {
+              columns: [
+                ['Total Number of Compliments', 50, 150, 80, 200, 140, 40]
+              ],
+              type: 'bar'
+            },
+            grid: {
+                x: {
+                    show: true
+                },
+                y: {
+                    show: true
+                }
+            }
+        });
+
+        var chart4 = c3.generate({
+            bindto: '#avg-complaint-time',
+            data: {
+              columns: [
+                ['Average Time Taken Per Case', 50, 150, 80, 200, 140, 40]
+              ],
+              type: 'bar'
+            },
+            grid: {
+                x: {
+                    show: true
+                },
+                y: {
+                    show: true
+                }
+            }
+        });
+        
 	});	
 }
