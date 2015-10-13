70b9622c87f8f18bff38dd602496334086c4a687
Ong Zhi Min <zhimin_93@hotmail.sg>
2015-10-13 15:49:13 +0800

    View Detailed Analytics

--- complaintApp/client/analytics/detailedAnalytics.js
+++ complaintApp/client/analytics/detailedAnalytics.js
@@ -17,10 +17,53 @@ if (Meteor.isClient) {
                 }
             },
             padding: {
-                top: 40,
-                right: 40,
+                top: 10,
+                right: 20,
                 left: 40,
             },
+        });
+
+        /*$("#advance-daterange span").html(moment().subtract("days", 29).format("MMMM D, YYYY") + " - " + moment().format("MMMM D, YYYY")), */
+        $("#advance-daterange").daterangepicker({
+            format: "MM/DD/YYYY",
+            startDate: moment().subtract(29, "days"),
+            endDate: moment(),
+            minDate: "01/01/2012",
+            maxDate: "12/31/2015",
+            dateLimit: {
+                days: 60
+            },
+            showDropdowns: !0,
+            showWeekNumbers: !0,
+            timePicker: !1,
+            timePickerIncrement: 1,
+            timePicker12Hour: !0,
+            ranges: {
+                Today: [moment(), moment()],
+                Yesterday: [moment().subtract(1, "days"), moment().subtract(1, "days")],
+                "Last 7 Days": [moment().subtract(6, "days"), moment()],
+                "Last 30 Days": [moment().subtract(29, "days"), moment()],
+                "This Month": [moment().startOf("month"), moment().endOf("month")],
+                "Last Month": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+            },
+            opens: "right",
+            drops: "down",
+            buttonClasses: ["btn", "btn-sm"],
+            applyClass: "btn-primary",
+            cancelClass: "btn-default",
+            separator: " to ",
+            locale: {
+                applyLabel: "Submit",
+                cancelLabel: "Cancel",
+                fromLabel: "From",
+                toLabel: "To",
+                customRangeLabel: "Custom",
+                daysOfWeek: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+                monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                firstDay: 1
+            }
+        }, function(a, t) {
+            $("#advance-daterange span").html(a.format("MMMM D, YYYY") + " - " + t.format("MMMM D, YYYY"))
         });
     });
 }
