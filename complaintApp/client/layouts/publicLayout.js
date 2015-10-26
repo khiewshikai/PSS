45b74edbf376d6ecaaecec411e441a07fcc780d4
Shi Kai <khiewshikai@hotmail.com>
2015-10-26 21:58:19 +0800

    updated banner

--- complaintApp/client/layouts/publicLayout.js
+++ complaintApp/client/layouts/publicLayout.js
@@ -1,6 +1,15 @@
 Template.publicLayout.helpers({
-    active: function(routeName) {
-        var curRoute = Router.current().route.getName();
-        return curRoute === routeName ? 'active' : '';
-    }
+	active: function(routeName) {
+		var curRoute = Router.current().route.getName();
+		return curRoute === routeName ? 'active' : '';
+	}
 })
+
+
+Template.publicLayout.events({
+
+	'click .navbar-nav li a':function(event, template) {
+		$(".navbar-collapse").collapse('hide');
+	}
+
+});
