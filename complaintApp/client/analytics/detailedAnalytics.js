e49bda418a84ee2cb3a7b43e10f3da14b9556c15
Ong Zhi Min <zhimin_93@hotmail.sg>
2015-10-30 09:27:32 +0800

    Download graph as png

--- complaintApp/client/analytics/detailedAnalytics.js
+++ complaintApp/client/analytics/detailedAnalytics.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
     Template.detailedAnalytics.onRendered(function(){
-        var chart1 = c3.generate({
+        /*var chart1 = c3.generate({
             bindto: '#analytics-space',
             data: {
               columns: [
@@ -21,7 +21,55 @@ if (Meteor.isClient) {
                 right: 20,
                 left: 40,
             },
-        });
+        });*/
+
+        var time = $("#time-interval").val();
+        var dateStart = "";
+        var dateEnd = "";
+        var tickFormat = "";
+        var xFormat = "";
+        var xAxisLabel = "";
+
+        function createGraph(dateStart, dateEnd, tickFormat, xFormat, xAxisLabel){
+            console.log("createGraph");
+            var graph = c3.generate({
+                bindto: '#analytics-space',
+                data: {
+                    x: 'x',
+                    xFormat: xFormat,
+                    columns: [
+                        ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                        ['y', 30, 200, 100, 400, 150, 250]
+                    ],
+                    type: 'bar',
+                },
+                axis: {
+                    x: {
+                        type: 'timeseries',
+                        label: {
+                            text: xAxisLabel,
+                            position: 'outer-right'
+                        },
+                        tick: {
+                            format: tickFormat
+                        }
+                    }
+                },
+                grid: {
+                    x: {
+                        show: true
+                    },
+                    y: {
+                        show: true
+                    }
+                },
+                padding: {
+                    top: 10,
+                    right: 20,
+                    left: 40,
+                }
+            });
+        }
 
         $("#month-start").datepicker({
             format: "yyyy-mm",
@@ -87,14 +135,12 @@ if (Meteor.isClient) {
                 firstDay: 1
             }
         }, function(a, t) {
-            $("#advance-daterange span").html(a.format("MMMM D, YYYY") + " - " + t.format("MMMM D, YYYY"))
+            $("#advance-daterange span").html(a.format("YYYY-MM-DD") + " - " + t.format("YYYY-MM-DD"))
         });
 
         /*$("#date-range").hide();*/
         $("#month-range").hide();
         $("#year-range").hide();
-
-        var time = $("#time-interval").val();
 
         $("#time-interval").change(function() {
             time = $("#time-interval").val();
@@ -112,5 +158,42 @@ if (Meteor.isClient) {
                 $("#year-range").show();
             }
         });
+
+        $("#view-button").click(function() {
+            if(time === "day"){
+                var date = $("#date-start-end").html();
+                dateStart = date.substr(0, 10);
+                dateEnd = date.substr(13);
+                xFormat = "%Y-%m-%d";
+                tickFormat = "%Y-%m-%d";
+                xAxisLabel = "Date(Day)";
+
+            } else if(time === "month"){
+                dateStart = $("#month-start").val();
+                dateEnd = $("#month-end").val();
+                xFormat = "%Y-%m";
+                tickFormat = "%Y-%m";
+                xAxisLabel = "Date(Month)";
+                
+            } else if(time === "year"){
+                dateStart = $("#year-start").val();
+                dateEnd = $("#year-end").val();
+                xFormat = '%Y';
+                tickFormat = "%Y";
+                xAxisLabel = "Date(Year)";
+            }
+
+            createGraph(dateStart, dateEnd, tickFormat, xFormat, xAxisLabel);
+        });
+
+
+        $("#downloadButton").click(function() {
+            var svg = $('svg');
+            svg.attr("id", "svg-id");
+
+            var chart = document.getElementById("svg-id");
+            saveSvgAsPng(chart, "diagram.png", {scale: 2});
+        });
+        
     });
 }
--- complaintApp/client/analytics/viewAnalytics.js
+++ complaintApp/client/analytics/viewAnalytics.js
@@ -8,13 +8,32 @@ if (Meteor.isClient) {
             zIndex: 999999
         }).disableSelection();
 
+        var xFormat = "%Y-%m-%d";
+        var tickFormat = "%Y-%m-%d";
+        var xAxisLabel = "Date(Day)";
+
         var chart1 = c3.generate({
             bindto: '#complaints',
             data: {
-              columns: [
-                ['Total Number of Complaints', 30, 200, 100, 400, 150, 250]
-              ],
-              type: 'bar'
+                x: 'x',
+                xFormat: xFormat,
+                columns: [
+                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                    ['Total Number of Complaints', 30, 200, 100, 400, 150, 250]
+                ],
+                type: 'bar'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    label: {
+                        text: xAxisLabel,
+                        position: 'outer-right'
+                    },
+                    tick: {
+                        format: tickFormat
+                    }
+                }
             },
             grid: {
                 x: {
@@ -29,10 +48,25 @@ if (Meteor.isClient) {
         var chart2 = c3.generate({
             bindto: '#complaints-per-cat',
             data: {
-              columns: [
-                ['Total Number of Complaints Per Category', 50, 150, 80, 200, 140, 40]
-              ],
-              type: 'bar'
+                x: 'x',
+                xFormat: xFormat,
+                columns: [
+                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                    ['Total Number of Complaints Per Category', 30, 200, 100, 400, 150, 250]
+                ],
+                type: 'bar'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    label: {
+                        text: xAxisLabel,
+                        position: 'outer-right'
+                    },
+                    tick: {
+                        format: tickFormat
+                    }
+                }
             },
             grid: {
                 x: {
@@ -47,10 +81,25 @@ if (Meteor.isClient) {
         var chart3 = c3.generate({
             bindto: '#compliments',
             data: {
-              columns: [
-                ['Total Number of Compliments', 50, 150, 80, 200, 140, 40]
-              ],
-              type: 'bar'
+                x: 'x',
+                xFormat: xFormat,
+                columns: [
+                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                    ['Total Number of Compliments', 30, 200, 100, 400, 150, 250]
+                ],
+                type: 'bar'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    label: {
+                        text: xAxisLabel,
+                        position: 'outer-right'
+                    },
+                    tick: {
+                        format: tickFormat
+                    }
+                }
             },
             grid: {
                 x: {
@@ -65,10 +114,25 @@ if (Meteor.isClient) {
         var chart4 = c3.generate({
             bindto: '#avg-complaint-time',
             data: {
-              columns: [
-                ['Average Time Taken Per Case', 50, 150, 80, 200, 140, 40]
-              ],
-              type: 'bar'
+                x: 'x',
+                xFormat: xFormat,
+                columns: [
+                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                    ['Average Time Taken Per Case', 30, 200, 100, 400, 150, 250]
+                ],
+                type: 'bar'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    label: {
+                        text: xAxisLabel,
+                        position: 'outer-right'
+                    },
+                    tick: {
+                        format: tickFormat
+                    }
+                }
             },
             grid: {
                 x: {
