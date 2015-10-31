e5ee52d42d8f584cf8b46ab2c4dca7ac4b6cd75f
Ong Zhi Min <zhimin_93@hotmail.sg>
2015-11-01 00:53:56 +0800

    Analytics function

--- complaintApp/client/analytics/viewAnalytics.js
+++ complaintApp/client/analytics/viewAnalytics.js
@@ -1,6 +1,68 @@
 if (Meteor.isClient) {
 	Template.viewAnalytics.onRendered(function(){
 
+        function getFormatDate(date){
+            var day = date.getDate();
+            var month = date.getMonth();
+            var year = date.getFullYear();
+
+            var finalDate = year + "-" + month + "-" + day;
+            return finalDate;
+        }
+
+        function getPastWeek(){
+            var week = ['x'];
+
+            var d = "";
+
+            for (i = 6; i >=1; i--){
+                d = new Date();
+                d.setDate(d.getDate() - i);
+                d = getFormatDate(d);
+                week.push(d);
+            }
+
+            d = getFormatDate(new Date());
+            week.push(d);
+
+            return week;
+        }
+
+        function getPastWeekComplaints(){
+            var totalComplaints = ['Total Number of Complaints'];
+
+            var noOfComplaintToday  = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date()}}).count();
+            totalComplaints.push(noOfComplaintToday);
+
+            for (i = 1; i <=7; i++) { 
+                var complaints = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-(i+1))), "$lte": new Date(new Date().setDate(new Date().getDate()-i))}}).count();
+                totalComplaints.push(complaints);
+            }
+
+            return totalComplaints;
+            /*var noOfComplaintToday  = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date()}}).count();     
+            var noOfComplaintPrevOneDay = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-2)), "$lte": new Date(new Date().setDate(new Date().getDate()-1))}}).count();
+            var noOfComplaintPrevDayTwo = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-3)), "$lte": new Date(new Date().setDate(new Date().getDate()-2))}}).count();
+            var noOfComplaintPrevDayThree = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-4)), "$lte": new Date(new Date().setDate(new Date().getDate()-3))}}).count();
+            var noOfComplaintPrevDayFour = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-5)), "$lte": new Date(new Date().setDate(new Date().getDate()-4))}}).count();
+            var noOfComplaintPrevDayFive = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-6)), "$lte": new Date(new Date().setDate(new Date().getDate()-5))}}).count();
+            var noOfComplaintPrevDaySix = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-7)), "$lte": new Date(new Date().setDate(new Date().getDate()-6))}}).count();*/
+        }
+
+        function getPastWeekCompliments () {
+            var totalCompliments = ['Total Number of Compliments'];
+
+            var noOfComplimentsToday  = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date()}}).count();
+            totalCompliments.push(noOfComplimentsToday);
+
+            for (i = 1; i <=7; i++) { 
+                var compliments = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-(i+1))), "$lte": new Date(new Date().setDate(new Date().getDate()-i))}}).count();
+                totalCompliments.push(compliments);
+            }
+
+            return totalCompliments;
+        }
+
         $(".ui-sortable").sortable({
             placeholder: "sort-highlight",
             connectWith: ".ui-sortable",
@@ -12,13 +74,64 @@ if (Meteor.isClient) {
         var tickFormat = "%Y-%m-%d";
         var xAxisLabel = "Date(Day)";
 
+        /*var chart = c3.generate({
+            bindto: '#complaints',
+            data: {
+                x: 'x',
+                columns: [
+                    ['x', 30, 50, 100, 230, 300, 310],
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 130, 300, 200, 300, 250, 450]
+                ]
+            }
+        });*/
+		
+		//var dateNow = new Date();
+		//new Date(new Date().setDate(new Date().getDate()-5))
+		//complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-1)), "$lte": new Date(new Date().setDate(new Date().getDate()-2))}}).count();
+		
+        /*var complaintsData = [];
+        complaintsData.push()*/
         var chart1 = c3.generate({
             bindto: '#complaints',
             data: {
                 x: 'x',
+                columns: [
+                    getPastWeek(),
+                    getPastWeekComplaints()
+                ],
+                type: 'bar'
+            },
+            grid: {
+                x: {
+                    show: true
+                },
+                y: {
+                    show: true
+                }
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    label: {
+                        text: xAxisLabel,
+                        position: 'outer-right'
+                    },
+                    tick: {
+                        format: tickFormat
+                    }
+                }
+            }
+        });
+
+
+        var chart2 = c3.generate({
+            bindto: '#complaints-per-cat',
+            data: {
+                x: 'x',
                 xFormat: xFormat,
                 columns: [
-                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                    getPastWeek(),
                     ['Total Number of Complaints', 30, 200, 100, 400, 150, 250]
                 ],
                 type: 'bar'
@@ -34,25 +147,6 @@ if (Meteor.isClient) {
                         format: tickFormat
                     }
                 }
-		
-		//var dateNow = new Date();
-		//new Date(new Date().setDate(new Date().getDate()-5))
-		//complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-1)), "$lte": new Date(new Date().setDate(new Date().getDate()-2))}}).count();
-		var noOfComplaintToday  = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date()}}).count();		
-		var noOfComplaintPrevOneDay = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-2)), "$lte": new Date(new Date().setDate(new Date().getDate()-1))}}).count();
-		var noOfComplaintPrevDayTwo = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-3)), "$lte": new Date(new Date().setDate(new Date().getDate()-2))}}).count();
-		var noOfComplaintPrevDayThree = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-4)), "$lte": new Date(new Date().setDate(new Date().getDate()-3))}}).count();
-		var noOfComplaintPrevDayFour = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-5)), "$lte": new Date(new Date().setDate(new Date().getDate()-4))}}).count();
-		var noOfComplaintPrevDayFive = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-6)), "$lte": new Date(new Date().setDate(new Date().getDate()-5))}}).count();
-        var noOfComplaintPrevDaySix = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-7)), "$lte": new Date(new Date().setDate(new Date().getDate()-6))}}).count();
-		
-        var chart1 = c3.generate({
-            bindto: '#complaints',
-            data: {
-              columns: [
-                ['Total Number of Complaints', noOfComplaintPrevDayFive, noOfComplaintPrevDayFour, noOfComplaintPrevDayThree, noOfComplaintPrevDayTwo, noOfComplaintPrevOneDay, noOfComplaintToday]
-              ],
-              type: 'bar'
             },
             grid: {
                 x: {
@@ -63,55 +157,15 @@ if (Meteor.isClient) {
                 }
             }
         });
-
-        var chart2 = c3.generate({
-            bindto: '#complaints-per-cat',
-            data: {
-                x: 'x',
-                xFormat: xFormat,
-                columns: [
-                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
-                    ['Total Number of Complaints Per Category', 30, 200, 100, 400, 150, 250]
-                ],
-                type: 'bar'
-            },
-            axis: {
-                x: {
-                    type: 'timeseries',
-                    label: {
-                        text: xAxisLabel,
-                        position: 'outer-right'
-                    },
-                    tick: {
-                        format: tickFormat
-                    }
-                }
-            },
-            grid: {
-                x: {
-                    show: true
-                },
-                y: {
-                    show: true
-                }
-            }
-        });
-		
-		var noOfComplimentsToday  = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date()}}).count();
-		var noOfComplimentsPrevOneDay = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-2)), "$lte": new Date(new Date().setDate(new Date().getDate()-1))}}).count();
-		var noOfComplimentsPrevDayTwo = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-3)), "$lte": new Date(new Date().setDate(new Date().getDate()-2))}}).count();
-		var noOfComplimentsPrevDayThree = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-4)), "$lte": new Date(new Date().setDate(new Date().getDate()-3))}}).count();
-		var noOfComplimentsPrevDayFour = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-5)), "$lte": new Date(new Date().setDate(new Date().getDate()-4))}}).count();
-		var noOfComplimentsPrevDayFive = complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-6)), "$lte": new Date(new Date().setDate(new Date().getDate()-5))}}).count();
-				
+			
         var chart3 = c3.generate({
             bindto: '#compliments',
             data: {
                 x: 'x',
                 xFormat: xFormat,
                 columns: [
-                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
-                    ['Total Number of Compliments', 30, 200, 100, 400, 150, 250]
+                    getPastWeek(),
+                    getPastWeekCompliments()
                 ],
                 type: 'bar'
             },
@@ -126,10 +180,6 @@ if (Meteor.isClient) {
                         format: tickFormat
                     }
                 }
-              columns: [
-                ['Total Number of Compliments', noOfComplimentsPrevDayFive, noOfComplimentsPrevDayFour, noOfComplimentsPrevDayThree, noOfComplimentsPrevDayTwo, noOfComplimentsPrevOneDay, noOfComplimentsToday]
-              ],
-              type: 'bar'
             },
             grid: {
                 x: {
@@ -147,7 +197,7 @@ if (Meteor.isClient) {
                 x: 'x',
                 xFormat: xFormat,
                 columns: [
-                    ['x', '2012-12-31', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05'],
+                    getPastWeek(),
                     ['Average Time Taken Per Case', 30, 200, 100, 400, 150, 250]
                 ],
                 type: 'bar'
@@ -172,19 +222,28 @@ if (Meteor.isClient) {
                     show: true
                 }
             }
+        });
+
+        $("#testButton").click(function() {
+            console.log("hello");
+            console.log(getPastWeek());
+            var test = noOfComplaintPrevOneDay = complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-2)), "$lte": new Date(new Date().setDate(new Date().getDate()-1))}}).count();
+            console.log(getPastWeekCompliments());
+            console.log(getPastWeekComplaints());
         });     
-	});
-	
-	Template.viewAnalytics.helpers({
-			totalNoOfComplaints: function() {
-				return complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-7)), "$lte": new Date(new Date().setDate(new Date().getDate()))}}).count();				
-			}
-	});
-	
-	Template.viewAnalytics.helpers({
-			totalNoOfCompliments: function() {
-				return complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-7)), "$lte": new Date(new Date().setDate(new Date().getDate()))}}).count();
-			}
-	});
+
+    });
+
+    Template.viewAnalytics.helpers({
+        totalNoOfComplaints: function() {
+            return complaintsCollection.find({"dateTimeOpen":{"$gte": new Date(new Date().setDate(new Date().getDate()-7)), "$lte": new Date(new Date().setDate(new Date().getDate()))}}).count();              
+        }
+    });
+    
+    Template.viewAnalytics.helpers({
+        totalNoOfCompliments: function() {
+            return complimentsCollection.find({"complimentTimeCreated":{"$gte": new Date(new Date().setDate(new Date().getDate()-7)), "$lte": new Date(new Date().setDate(new Date().getDate()))}}).count();
+        }
+    });
 	
 }
