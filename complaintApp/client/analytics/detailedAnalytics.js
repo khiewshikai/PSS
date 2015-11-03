4a9ab3adaf017f15027978274c34b657dad15e19
Ong Zhi Min <zhimin_93@hotmail.sg>
2015-11-03 17:56:28 +0800

    Changes to view Detailed Analytics

--- complaintApp/client/analytics/detailedAnalytics.js
+++ complaintApp/client/analytics/detailedAnalytics.js
@@ -11,6 +11,14 @@ Template.detailedAnalytics.onRendered(function(){
     function getFormatDate(date){
         var day = date.getDate();
         var month = date.getMonth() + 1;
+        if(day < 10){
+            day = "0" + day;
+        }
+
+        if(month < 10){
+            month = "0" + month;
+        }
+        
         var year = date.getFullYear();
         var finalDate = year + "-" + month + "-" + day;
         return finalDate;
--- complaintApp/client/analytics/viewAnalytics.js
+++ complaintApp/client/analytics/viewAnalytics.js
@@ -4,6 +4,13 @@ if (Meteor.isClient) {
         function getFormatDate(date){
             var day = date.getDate();
             var month = date.getMonth() + 1;
+            if(day < 10){
+                day = "0" + day;
+            }
+
+            if(month < 10){
+                month = "0" + month;
+            }
             var year = date.getFullYear();
 
             var finalDate = year + "-" + month + "-" + day;
